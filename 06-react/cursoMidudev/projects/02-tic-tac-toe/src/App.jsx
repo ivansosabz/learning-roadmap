522896cbf4657f1bdc02d43c07e0805c289d5f8d
ivansosabz <ivansosabz@gmail.com>
2026-06-30 23:46:38 -0300

    feat: turnos y board 0h15m

--- 06-react/cursoMidudev/projects/02-tic-tac-toe/src/App.jsx
+++ 06-react/cursoMidudev/projects/02-tic-tac-toe/src/App.jsx
@@ -1,8 +1,46 @@
 import { useState } from 'react'
 import './App.css'
 
+const TURNS = {
+  x: 'x',
+  o: 'o'
+}
+
+const Square = ({children, updateBoard, index}) => {
+  return (
+      <div className="square">
+        {children}
+      </div>
+  )
+}
+
 function App() {
-  return <h1>Tic Tac Toe</h1>
+    const [board, setBoard] = useState(Array(9).fill(null))
+    const [turn, setTurns] = useState(TURNS.x)
+    return (
+        <main className="board">
+            <h1>Tic Tac Toe</h1>
+            <section className="game">
+                {
+                    board.map((_, index) => {
+                        return (
+                            <Square key={index} index={index}>
+                              {board[index]}
+                            </Square>
+                        )
+                    })
+                }
+            </section>
+            <section className="turn">
+                <Square isSelected={turn=== TURNS.x}>
+                    {TURNS.x}
+                </Square>
+                <Square isSelected={turn=== TURNS.o}>
+                    {TURNS.o}
+                </Square>
+            </section>
+        </main>
+    )
 }
 
 export default App
